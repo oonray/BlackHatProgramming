1760b942610afa9fe323589a29bf163efb39b2cd
oonray <alexanderbjornsrud@gmail.com>
2021-10-03 23:58:29 +0200

    .

---
+++ BlackHatGo/08.reverseproxy/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"github.com/gorilla/mux"
+	log "github.com/Sirupsen/logrus"
+)
+
+var (
+	hostProxy 	= make(map[string]string)
+	proxies		= make(map[string]*httputil.ReverseProxy)
+)
+
+func init(){
+	hostProxy["attacker1.com"] = "http://10.0.1.20:10080"
+	hostProxy["attacker2.com"] = "http://10.0.1.20:20080"
+	for k,v := range hostProxy {
+		remote, err := url.Parse(v)
+		if (err!=nil){
+			log.Fatalf("Unable To Parse Proxy Target")
+		}
+		proxies[k] = httputil.NewSingleHostReverseProxy(remote)
+	}
+}
+
+func main() {
+	r = mux.NewRouter()
+
+}
