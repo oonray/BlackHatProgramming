962164703df4eb942f96ef4b72bf073c05234c80
oonray <alexanderbjornsrud@gmai.com>
2022-01-12 17:56:54 +0100

    .

--- BlackHatGo/Chisel/server.go
+++ BlackHatGo/Chisel/server.go
@@ -2,55 +2,56 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"runtime"
-	"strconv"
 	"strings"
 	"time"
 
 	server "github.com/jpillora/chisel/server"
-	share "github.com/jpillora/chisel/share"
 	cos "github.com/jpillora/chisel/share/cos"
 )
 
 var (
-	config server.Config = &server.Config{}
+	config *server.Config = &server.Config{}
 	host *string
 	p  *string
 	port *string
-	pid *bool
 	verbose *bool
 )
 
+type multiFlag struct {
+	values *[]string
+}
+
+func (flag multiFlag) String() string {
+	return strings.Join(*flag.values, ", ")
+}
+
+func (flag multiFlag) Set(arg string) error {
+	*flag.values = append(*flag.values, arg)
+	return nil
+}
+
 func main(){
-	flags.StringVar(&config.KeySeed, "key", "", "")
-	flags.StringVar(&config.AuthFile, "authfile", "", "")
-	flags.StringVar(&config.Auth, "auth", "", "")
-	flags.DurationVar(&config.KeepAlive, "keepalive", 25*time.Second, "")
-	flags.StringVar(&config.Proxy, "proxy", "", "")
-	flags.StringVar(&config.Proxy, "backend", "", "")
-	flags.BoolVar(&config.Socks5, "socks5", false, "")
-	flags.BoolVar(&config.Reverse, "reverse", false, "")
-	flags.StringVar(&config.TLS.Key, "tls-key", "", "")
-	flags.StringVar(&config.TLS.Cert, "tls-cert", "", "")
-	flags.Var(multiFlag{&config.TLS.Domains}, "tls-domain", "")
-	flags.StringVar(&config.TLS.CA, "tls-ca", "", "")
+	flag.StringVar(&config.KeySeed, "key", "", "")
+	flag.StringVar(&config.AuthFile, "authfile", "", "")
+	flag.StringVar(&config.Auth, "auth", "", "")
+	flag.DurationVar(&config.KeepAlive, "keepalive", 25*time.Second, "")
+	flag.StringVar(&config.Proxy, "proxy", "", "")
+	flag.StringVar(&config.Proxy, "backend", "", "")
+	flag.BoolVar(&config.Socks5, "socks5", false, "")
+	flag.BoolVar(&config.Reverse, "reverse", false, "")
+	flag.StringVar(&config.TLS.Key, "tls-key", "", "")
+	flag.StringVar(&config.TLS.Cert, "tls-cert", "", "")
+	flag.Var(multiFlag{&config.TLS.Domains}, "tls-domain", "")
+	flag.StringVar(&config.TLS.CA, "tls-ca", "", "")
 
-	host = flags.String("host", "", "")
-	p = flags.String("p", "", "")
-	port = flags.String("port", "", "")
-	pid = flags.Bool("pid", false, "")
-	verbose = flags.Bool("v", false, "")
+	host = flag.String("host", "", "")
+	p = flag.String("p", "", "")
+	port = flag.String("port", "", "")
+	verbose = flag.Bool("v", false, "")
 
-	flags.Usage = func() {
-		fmt.Print(serverHelp)
-		os.Exit(0)
-	}
-	flags.Parse(args)
+	flag.Parse()
 
 	if *host == "" {
 		*host = os.Getenv("HOST")
@@ -70,14 +71,11 @@ func main(){
 	if config.KeySeed == "" {
 		config.KeySeed = os.Getenv("CHISEL_KEY")
 	}
-	s, err := chserver.NewServer(config)
+	s, err := server.NewServer(config)
 	if err != nil {
 		log.Fatal(err)
 	}
 	s.Debug = *verbose
-	if *pid {
-		generatePidFile()
-	}
 	go cos.GoStats()
 	ctx := cos.InterruptContext()
 	if err := s.StartContext(ctx, *host, *port); err != nil {
