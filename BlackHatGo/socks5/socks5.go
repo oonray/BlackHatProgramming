7d27c08037a6f821f83e48a183be240fa1b66936
oonray <alexanderbjornsrud@gmai.com>
2022-01-14 18:52:43 +0100

    .

---
+++ BlackHatGo/socks5/socks5.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"errors"
+	"github.com/gorilla/mux"
+    "github.com/things-go/go-socks5"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	lp *string
+)
+
+func argparse(){
+	lp = flag.String("lp","","Port to listen to")
+	flag.Parse()
+}
+
+func main() {
+	server := socks5.NewServer(
+		socks5.WithLogger(
+			socks5.NewLogger(log.New()),
+		),
+	)
+
+	if err := server.ListenAndServe("tcp",fmt.Sprintf(":%s",lp));err != nil {
+		log.Fatalf("%s",err)
+	}
+}
