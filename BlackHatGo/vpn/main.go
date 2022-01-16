51ab0b0f213b07aa842545cabb529d30f5c5747b
oonray <alexanderbjornsrud@gmai.com>
2022-01-16 20:52:55 +0100

    .

--- BlackHatGo/vpn/main.go
+++ BlackHatGo/vpn/main.go
@@ -14,6 +14,38 @@ var (
 	creds openvpn3.UserCredentials = openvpn3.UserCredentials{}
 )
 
+type callbacks interface {
+	openvpn3.Logger
+	openvpn3.EventConsumer
+	openvpn3.StatsConsumer
+}
+
+type loggingCallbacks struct {
+}
+
+func (lc *loggingCallbacks) Log(text string) {
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		fmt.Println("Openvpn log >>", line)
+	}
+}
+
+func (lc *loggingCallbacks) OnEvent(event openvpn3.Event) {
+	fmt.Printf("Openvpn event >> %+v\n", event)
+}
+
+func (lc *loggingCallbacks) OnStats(stats openvpn3.Statistics) {
+	fmt.Printf("Openvpn stats >> %+v\n", stats)
+}
+
+var _ callbacks = &loggingCallbacks{}
+
+type StdoutLogger func(text string)
+
+func (lc StdoutLogger) Log(text string) {
+	lc(text)
+}
+
 func argparse() error {
 	vpn_file = flag.String("vp","","The Vpn file to use")
 	flag.StringVar(&creds.Username,"u","","The username to use")
@@ -34,6 +66,15 @@ func main(){
 		log.Error(err)
 		return 
 	}
+
+	var logger StdoutLogger = func(text string) {
+		lines := strings.Split(text, "\n")
+		for _, line := range lines {
+			fmt.Println("Library check >>", line)
+		}
+	}
+
+	openvpn3.SelfCheck(logger)
  
 	bytes, err := ioutil.ReadFile(*vpn_file)
 	if err != nil {
