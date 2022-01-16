c8a88481d7fad919514453dc5b8026750ff92cee
oonray <alexanderbjornsrud@gmai.com>
2022-01-16 21:23:08 +0100

    .

--- BlackHatGo/vpn/main.go
+++ BlackHatGo/vpn/main.go
@@ -5,51 +5,17 @@ import (
 	"flag"
 	"errors"
 	"io/ioutil"
-	"github.com/mysteriumnetwork/go-openvpn/openvpn3"
+	openvpn "github.com/lighthouse8/go-openvpn"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	vpn_file *string	
-	creds openvpn3.UserCredentials = openvpn3.UserCredentials{}
+	vpn_file *string
+	config *openvpn.Config
 )
-
-type callbacks interface {
-	openvpn3.Logger
-	openvpn3.EventConsumer
-	openvpn3.StatsConsumer
-}
-
-type loggingCallbacks struct {
-}
-
-func (lc *loggingCallbacks) Log(text string) {
-	lines := strings.Split(text, "\n")
-	for _, line := range lines {
-		fmt.Println("Openvpn log >>", line)
-	}
-}
-
-func (lc *loggingCallbacks) OnEvent(event openvpn3.Event) {
-	fmt.Printf("Openvpn event >> %+v\n", event)
-}
-
-func (lc *loggingCallbacks) OnStats(stats openvpn3.Statistics) {
-	fmt.Printf("Openvpn stats >> %+v\n", stats)
-}
-
-var _ callbacks = &loggingCallbacks{}
-
-type StdoutLogger func(text string)
-
-func (lc StdoutLogger) Log(text string) {
-	lc(text)
-}
 
 func argparse() error {
 	vpn_file = flag.String("vp","","The Vpn file to use")
-	flag.StringVar(&creds.Username,"u","","The username to use")
-	flag.StringVar(&creds.Password,"p","","The password to use")
 	flag.Parse()
 
 	if *vpn_file == "" {
@@ -67,28 +33,7 @@ func main(){
 		return 
 	}
 
-	var logger StdoutLogger = func(text string) {
-		lines := strings.Split(text, "\n")
-		for _, line := range lines {
-			fmt.Println("Library check >>", line)
-		}
-	}
+	config = config.NewConfig()
+	config.LoadFile(*vpn_file)
 
-	openvpn3.SelfCheck(logger)
- 
-	bytes, err := ioutil.ReadFile(*vpn_file)
-	if err != nil {
-		log.Error(err)
-		return
-	}
-
-	config := openvpn3.NewConfig(string(bytes))
-	session := openvpn3.NewSession(config, creds, &loggingCallbacks{})
-
-	session.Start()
-	err = session.Wait()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
 }
