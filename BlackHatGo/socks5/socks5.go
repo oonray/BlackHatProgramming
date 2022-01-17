6c40a103cc8f9d9f5973104757b42accfdcf6e26
oonray <alexanderbjornsrud@gmai.com>
2022-01-17 10:03:06 +0100

    .

--- BlackHatGo/socks5/socks5.go
+++ BlackHatGo/socks5/socks5.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"fmt"
 	"flag"
+	"errors"
     "github.com/things-go/go-socks5"
 	logR "github.com/sirupsen/logrus"
 )
@@ -13,12 +14,23 @@ var (
 	lp *string
 )
 
-func argparse(){
+func argparse()error{
 	lp = flag.String("lp","","Port to listen to")
 	flag.Parse()
+
+	if *lp == "" {
+		return errors.New("Listen port must be declared")
+	}	
+	return nil
 }
 
 func main() {
+	err := argparse()
+	if err != nil {
+		logR.Error(err)
+		return
+	}
+
 	server := socks5.NewServer(
 		socks5.WithLogger(socks5.NewLogger(log.New(os.Stdout, "socks5: ", log.LstdFlags))),
 	)
@@ -27,5 +39,3 @@ func main() {
 		logR.Fatalf("%s",err)
 	}
 }
-
-//uh
