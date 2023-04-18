92fcefa302049cc8aa78c0edf97203dbe5a6ecbe
oonray <alexanderbjornsrud@gmail.com>
2023-04-18 13:39:54 +0200

    .

---
+++ NTLM/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <pair>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <caaa/argparse.h>
+#include <curl/curl.h>
+
+std::vector<std::thread *> *threads;
+std::vector<std::string *> *user;
+std::vector<std::string *> *pass;
+
+int main(int argc, char *argv[]) {
+  threads = new std::vector<std::thread *>;
+  return 0;
+}
