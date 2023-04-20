5f3dcb0fcda7124196a11ceb3059d18ab25156a3
oonray <alexanderbjornsrud@gmail.com>
2023-04-20 15:21:38 +0200

    .

--- NTLM/src/main.cpp
+++ NTLM/src/main.cpp
@@ -1,17 +1,19 @@
+#include <curl/curl.h>
 #include <iostream>
 #include <pair>
 #include <string>
 #include <thread>
+#include <utility>
 #include <vector>
 
-#include <caaa/argparse.h>
 #include <curl/curl.h>
 
 std::vector<std::thread *> *threads;
-std::vector<std::string *> *user;
-std::vector<std::string *> *pass;
+std::vector<std::pair<std::string, std::string> *> creds;
 
-int main(int argc, char *argv[]) {
+void Check(std::string user, std::string pass)
+
+    int main(int argc, char *argv[]) {
   threads = new std::vector<std::thread *>;
   return 0;
 }
