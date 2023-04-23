c5eb11008a7b7ec56a6cb65684bfa38fe3fa755f
oonray <alexanderbjornsrud@gmail.com>
2023-04-23 13:41:12 +0200

    .

--- NTLM/src/main.cpp
+++ NTLM/src/main.cpp
@@ -1,24 +1,42 @@
 #include <curl/curl.h>
+#include <curl/easy.h>
 #include <iostream>
 #include <string>
 #include <thread>
+#include <unistd.h>
 #include <utility>
 #include <vector>
 
-#include <curl/curl.h>
-
 std::vector<std::thread *> *threads;
-<<<<<<< HEAD
 std::vector<std::pair<std::string, std::string> *> creds;
-=======
-std::vector<std::string *> *user;
 
-std::vector<std::string *> *pass;
->>>>>>> 17925f5c4e49b8f158e8367eba5a0d184b5f9c04
+void Check(std::string user, std::string pass) { CURL *h = curl_easy_init(); }
 
-void Check(std::string user, std::string pass)
-
-    int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   threads = new std::vector<std::thread *>;
+
+  for (;;) {
+    switch (getopt(argc, argv, "w:p:u:ht")) {
+    case 'w':
+      printf("switch 'w' specified\n");
+      continue;
+    case 'p':
+      printf("switch 'p' specified\n");
+      continue;
+    case 'u':
+      printf("switch 'u' specified\n");
+      continue;
+    case '?':
+    case 'h':
+    default:
+      printf("Help/Usage Example\n");
+      break;
+
+    case -1:
+      break;
+    }
+
+    break;
+  }
   return 0;
 }
