b223316d4631d6b7f291492b69006d5371b2a4f5
oonray <alexanderbjornsrud@gmail.com>
2023-04-26 17:00:23 +0200

    .

--- NTLM/src/request.c
+++ NTLM/src/request.c
@@ -30,12 +30,19 @@ void *valid_user(void *arg) {
   check(arg != NULL, "no arg provided");
   UserParam *v = (UserParam *)arg;
 
-  ca_io_stream_io_read_pipe(v->p, CA_OUT);
-  bstring out = ca_io_stream_buff_read_pipe(v->p, CA_OUT);
-
-  while (bstrcmp(out, bfromcstr("%&DONE!")) != 0) {
-    ca_vector_push(v->v, out);
+  int open = ca_io_stream_pipe_open(v->p, CA_OUT);
+  while (open != -1) {
+    ca_io_stream_io_read_pipe(v->p, CA_OUT);
     bstring out = ca_io_stream_buff_read_pipe(v->p, CA_OUT);
+
+    while (bstrcmp(out, bfromcstr("%&DONE!")) != 0) {
+      ca_vector_push(v->v, out);
+      bstring out = ca_io_stream_buff_read_pipe(v->p, CA_OUT);
+      int open = ca_io_stream_pipe_open(v->p, CA_OUT);
+      if (open == -1) {
+        return arg;
+      }
+    }
   }
   return arg;
 error:
